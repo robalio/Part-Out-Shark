bda2fcca8f283dffb1972be487cc2d66f7a10a63
robalio <robbieaalio@gmail.com>
2026-05-27 00:23:40 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/p158/',
+  base: '/Part-Out-Shark/',
   plugins: [react()],
   build: { outDir: "docs" }
 })
